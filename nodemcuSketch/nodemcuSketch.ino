dcbeb08e6962bacc640bc7edf3714ee4335f9fd9
charlesGdrt <charles.goudaert@isen.yncrea.fr>
2019-02-13 08:45:19 +0100

    WiFi setup page and Sigfox test
    WiFi setup page that display ID and PAC for Sigfox communication. Send Sigfox message attempt n1.

--- nodemcuSketch/nodemcuSketch.ino
+++ nodemcuSketch/nodemcuSketch.ino
@@ -1,36 +1,76 @@
-// DHT11 sensor
-#include <dht.h>
+#include <dht.h>              // DHT11 sensor
+#include <SoftwareSerial.h>   // Sigfox communication by antena
 
-// Sigfox communication
-#include <SoftwareSerial.h>
-#include <SigFox.h>
+#include <ESP8266WiFi.h>      // WiFi access point
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+// Set the ssid of the setup page
+#ifndef APSSID
+#define APSSID "WeatherStation"
+#endif
+
+const char *ssid = APSSID;
+ESP8266WebServer server(80);
 
 
 dht DHT;
 
-// pin out of DHT11 module goes to D4 pin on the Nodemcu
-#define DHT11_PIN D4
+#define DHT11_PIN D4          // pin out of DHT11 module goes to D4 pin on the Nodemcu
 
-// pin for Sigfox module
-#define RxNodePin 13
+#define RxNodePin 13          // pin for Sigfox module
 #define TxNodePin 15
 
-// Setup UART Communication with 
-SoftwareSerial Sigfox =  SoftwareSerial(RxNodePin, TxNodePin);
+SoftwareSerial Sigfox =  SoftwareSerial(RxNodePin, TxNodePin);  // Setup UART Communication with 
+uint8_t sigfoxMsg[12];        // 12 bytes message buffer
 
-// 12 bytes message buffer
-uint8_t sigfoxMsg[12];
-
-// variables to store temperature and humidity
-int T;
+int T;                        // variables to store temperature and humidity
 int H;
 
+String IDSigfox;
+String PACSigfox; 
+
+
+void setup(){
+  pinMode(RxNodePin, INPUT);
+  pinMode(TxNodePin, OUTPUT);
+  
+  Serial.begin(115200);
+  Sigfox.begin(9600);
+  
+  Serial.println("\nSigfox parameters :");
+  IDSigfox = getID();
+  Serial.print("Device ID: " + IDSigfox); 
+  PACSigfox = getPAC();
+  Serial.print("Device PAC Number: " + PACSigfox);
+
+  Serial.print("The setup page is available at 192.168.4.1 in a web browser");
+
+  webSetupPage();
+}
+
+void loop(){
+  acquireDHT();
+    
+  Serial.println(T);
+  Serial.println(H);
+  
+  sendDataSigfox(T, H);
+}
+
+
+
+
+
+/* acquire temperature and humidity 
+ * from the DHT11 module 
+ * and cast the data into integer 
+ */
 void acquireDHT(){
-  // acquire temperature and humidity from the DHT11 module and cast the data into integer
-  // Humidity Range: 20-90% RH
-  // Humidity Accuracy: ±5% RH
-  // Temperature Range: 0-50 °C
-  // Temperature Accuracy: ±2% °C
+  /* Humidity Range: 20-90% RH
+   * Humidity Accuracy: ±5% RH
+   * Temperature Range: 0-50 °C
+   * Temperature Accuracy: ±2% °C
+   */
   int chk = DHT.read11(DHT11_PIN);
   if(chk>=0){
     T = (int) DHT.temperature;
@@ -39,54 +79,25 @@ void acquireDHT(){
 }
 
 
-void setup()
-{
-  pinMode(RxNodePin, INPUT);
-  pinMode(TxNodePin, OUTPUT);
-  // setup serial connection with 115200 baud rate
-  Serial.begin(115200);
-  Sigfox.begin(9600);
-
-  Serial.println("\nSigfox parameters :");
-  Serial.print("Device ID: " + getID()); 
-  Serial.print("Device PAC Number: " + getPAC());
-}
-
-void loop()
-{
-  acquireDHT();
-    
-  Serial.println(T);
-  Serial.println(H);
-  
-  sendMessage(T+"/"+H);
-  delay(1000*30);
-  
-}
-
-
-void sendMessage(String sigfoxMsg) {
+// Send data to Sigfox 
+void sendDataSigfox(int T, int H) {
   //Sigfox.print("AT$SF=");
-  SigFox.begin();
-  SigFox.beginPacket();
-  SigFox.print(sigfoxMsg);
-  SigFox.endPacket();
-  
-
+  Sigfox.print(T + "/" + H);
   Sigfox.print("\r");
-
+  delay(1000*30);     // delay to be in the low
 }
 
 
 
+
+// Get Sigfox informations
 
 // Get device ID
 String getID () {
   String deviceId = "";
   char sigfoxBuffer;
 
-  // Send AT$I=10 to WISOL to GET ID number
-  Sigfox.print("AT$I=10\r");
+  Sigfox.print("AT$I=10\r");    // Send AT$I=10 to WISOL to GET ID number
 
   while (!Sigfox.available()){
      delay(10);
@@ -106,8 +117,7 @@ String getPAC (){
   String pacNumber = "";
   char sigfoxBuffer;
 
-  // Send AT$I=11 to WISOL to GET PAC number
-  Sigfox.print("AT$I=11\r");
+  Sigfox.print("AT$I=11\r");    // Send AT$I=11 to WISOL to GET PAC number
   while (!Sigfox.available()){
      delay(10);
   }
@@ -117,4 +127,28 @@ String getPAC (){
     delay(10);
   }
   return pacNumber;
+}
+
+
+
+
+// setup page
+
+void handleRoot() {
+  // setup page
+  String page = "<h1>Welcome on our Weather station setup page !</h1></BR><h4>ID : "+String(IDSigfox)+"</h4></BR><h4>PAC : "+String(PACSigfox)+"</h4>";
+  server.send(200, "text/html", page);
+}
+// Create the access point, show the setup page for 40s and then delete it 
+void webSetupPage(){
+  WiFi.softAP(ssid);
+  server.on("/", handleRoot);
+  server.begin();
+  
+  for(int i=0;i<=20;i++){
+    server.handleClient();
+    delay(1000*2);
+  }
+  // Close the access point
+  WiFi.softAPdisconnect (true);
 }
