1b55b32c9e46ea88da5b787c4fe190d14c1925aa
QuentinConsigny <43677523+QuentinConsigny@users.noreply.github.com>
2019-02-25 15:24:01 +0100

    Update of data_show
    Update of data_show_on_map.py.
    In order to view a .nc file, put in a first folder : the python script + a folder named "examples"
    In the folder "examples" put a .nc named file.nc

--- data_show_on_map.py
+++ data_show_on_map.py
@@ -5,25 +5,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-# plot rainfall from NWS using special precipitation
-# colormap used by the NWS, and included in basemap.
 
-nc = NetCDFFile('examples/nws_precip_conus_20061222.nc')
-# data from http://water.weather.gov/precip/
+nc = NetCDFFile('examples/file.nc')
+#Source
+#http://water.weather.gov/precip/
+
 prcpvar = nc.variables['amountofprecip']
 data = 0.01*prcpvar[:]
-print("la data :")
-print(prcpvar)
-print(data[1][0])
-print(len(data[0]))
 
 #In order to put new values
-for i in range(len(data)):
-	for j in range(len(data[0])):
-		#data[i][j]=random.random()*50
-		if (600<i and i<850):
-			if(650<j and j<700):
-				data[i][j]=70
+for i in range (620,630):
+    for j in range (754,766):
+        data[i][j]=50
 
 
 latcorners = nc.variables['lat'][:]
@@ -33,11 +26,10 @@ lat_0 = nc.variables['true_lat'].getValue()
 # create figure and axes instances
 fig = plt.figure(figsize=(8,8))
 ax = fig.add_axes([0.1,0.1,0.8,0.8])
-# create polar stereographic Basemap instance.
-m = Basemap(projection='stere',lon_0=lon_0,lat_0=90.,lat_ts=lat_0,\
-            llcrnrlat=latcorners[0],urcrnrlat=latcorners[2],\
-            llcrnrlon=loncorners[0],urcrnrlon=loncorners[2],\
-            rsphere=6371200.,resolution='l',area_thresh=10000)
+
+m = Basemap(resolution='l',satellite_height=4000000.,projection='nsper',lat_0 = 35., lon_0 = -20.,llcrnrx=400000.,llcrnry=500000.,urcrnrx=2700000.,urcrnry=2700000.)
+
+
 # draw coastlines, state and country boundaries, edge of map.
 m.drawcoastlines()
 m.drawstates()
@@ -52,12 +44,33 @@ ny = data.shape[0]; nx = data.shape[1]
 lons, lats = m.makegrid(nx, ny) # get lat/lons of ny by nx evenly space grid.
 x, y = m(lons, lats) # compute map proj coordinates.
 # draw filled contours.
-clevs = [0,1,2.5,5,7.5,10,15,20,30,40,50,70,100,150,200,250,300,400,500,600,750]
+
+
+#Select values on lader
+#clevs = [0,1,2.5,5,7.5,10,15,20,30,40,50,70,100,150,200,250,300,400,500,600,750]
+#clevs = []
+clevs=[0,1,2,3,4,5,7.5,10,12.5,15,20,25,30,35,40,50,75,100,200]
+
+'''
+for i in range(0,5):
+    clevs.append(i)
+for i in range(0,5):
+    clevs.append(5+2*i)
+for i in range(0,5):
+    clevs.append(15+5*i)
+'''
+
 cs = m.contourf(x,y,data,clevs,cmap=cm.s3pcpn)
+
+
 # add colorbar.
 cbar = m.colorbar(cs,location='bottom',pad="5%")
-cbar.set_label('mm')
+cbar.set_label('C')
 # add title
-plt.title(prcpvar.long_name+' for period ending '+prcpvar.dateofdata)
+plt.title("Show Example"+' for period ending '+prcpvar.dateofdata[7:9]+"/"+prcpvar.dateofdata[5:7]+"/"+prcpvar.dateofdata[0:4])
 plt.show()
 
+print(nc.variables['lat'][:])
+print(nc.variables['lon'][:])
+print(nc.variables['true_lon'].getValue())
+print(nc.variables['true_lat'].getValue())
