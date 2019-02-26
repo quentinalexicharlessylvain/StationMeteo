7ba8138ab5ff1a264eae5d226162e441cbc5df77
SlyLou <sylvain.louvet@isen.yncrea.fr>
2019-02-26 18:15:18 +0100

    HTML changement

--- serveur.py
+++ serveur.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_file
 from flask import request
 from flask import render_template
 from flask import jsonify
@@ -6,8 +6,12 @@ import sqlite3
 import datetime
 from flask_cors import CORS
 import os
+
 app = Flask(__name__, static_url_path='')
 CORS(app)
+
+temperature = []
+humidity = []
 
 app._static_folder = os.path.abspath('')
 
@@ -22,20 +26,39 @@ app._static_folder = os.path.abspath('')
 # 			print(body['data'][1])
 # 		return jsonify(body)
 
-@app.route("/")
+def decode(receivedData):
+    if(len(receivedData)!=8):
+        return -1,-1
+
+    odd = False
+    data = []
+
+    for number in receivedData:
+        if(odd):
+            data += number
+        odd = not odd
+
+    return data[0]+data[1], data[2]+data[3]
+
+decodage = decode("32343139")
+temperature.append(decodage[0])
+humidity.append(decodage[1])
+
+
+@app.route("/", methods=['GET'])
 def root():
-	root_dir = os.path.dirname(os.getcwd())
-	return app.send_from_diretory(root_dir, "index.html")
+	return send_file(filename_or_fp = "index.html", mimetype = "text/html")
 
 
-@app.route("/temps", methods=['GET'])
-def gettemp():
+@app.route("/releve", methods=['GET'])
+def getreleve():
 	if request.method=='GET':
-		temperatures = [
-		{'data': 5, 't': datetime.datetime.utcnow()},
-		{'data': 5, 't': datetime.datetime.utcnow()},
-		{'data': 5, 't': datetime.datetime.utcnow()}
+		releves = [
+		{'name': 1, 'temp': temperature[0], 'humid': humidity[0], 't': datetime.datetime.utcnow()},
+		{'name': 2, 'temp': 7, 'humid': 8, 't': datetime.datetime.utcnow()},
+		{'name': 3, 'temp': 9, 'humid': 9, 't': datetime.datetime.utcnow()}
 		]
-		return jsonify(temperatures)
+		return jsonify(releves)
+
 
 app.run(debug=True)
