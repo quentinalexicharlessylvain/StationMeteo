685dab388e11205f0403d696555b24a076fd0f44
SlyLou <sylvain.louvet@isen.yncrea.fr>
2019-03-01 11:41:14 +0100

    test

--- serveur.py
+++ serveur.py
@@ -7,24 +7,6 @@ import datetime
 from flask_cors import CORS
 import os
 
-app = Flask(__name__, static_url_path='')
-CORS(app)
-conn = sqlite3.connect('weather_station.db')
-
-cursor = conn.cursor()
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS weather(
-     name INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
-     temp INTEGER,
-     humid INTEGER,
-     date DATETIME2
-)
-""")
-conn.commit()
-
-
-app._static_folder = os.path.abspath('')
-
 def decode(receivedData):
     if(len(receivedData)!=8):
         return -1,-1
@@ -39,6 +21,32 @@ def decode(receivedData):
 
     return data[0]+data[1], data[2]+data[3]
 
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
+
+app = Flask(__name__, static_url_path='')
+CORS(app)
+conn = sqlite3.connect('weather_station.db', check_same_thread=False)
+conn.row_factory = dict_factory
+cursor = conn.cursor()
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS weather(
+     name INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+     temp INTEGER,
+     humid INTEGER,
+     date VARCHAR
+)
+''')
+
+
+
+app._static_folder = os.path.abspath('')
+
+
+
 @app.route("/", methods=['POST'])
 def data():
     if request.method=='POST':
@@ -46,12 +54,16 @@ def data():
         if body == None:
             return "not a json"
         if 'data' in body.keys():
-            decodage = decode(body['data'][1])
+            decodage = decode(body['data'])
             temperature = int(decodage[0])
             humidity = int (decodage[1])
-            cursor.execute("""
-                INSERT INTO weather(temp, humid, date) VALUES(?, ?, SYSDATETIME ())""", (temperature, humidity))
-            print(body['data'][1])
+            date = datetime.datetime.utcnow()
+            cursor.execute('''
+                INSERT INTO weather(temp,humid,date) VALUES(?,?,?)''', (temperature, humidity, date))
+            cursor.execute("""SELECT name, temp, humid, date FROM weather""")
+            releves = cursor.fetchall()
+            print(releves)
+            print(body['data'])
         return jsonify(body)
 
 
@@ -69,3 +81,5 @@ def getreleve():
 
 
 app.run(debug=True)
+conn.commit()
+conn.close()
