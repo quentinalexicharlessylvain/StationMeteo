333c81e936a48adf7e4de013d5336833a9755ab0
QuentinConsigny <43677523+QuentinConsigny@users.noreply.github.com>
2019-03-03 17:21:15 +0100

    Script commenté

--- data_show_on_map.py
+++ data_show_on_map.py
@@ -1,23 +1,23 @@
 from mpl_toolkits.basemap import Basemap, cm
-# requires netcdf4-python (netcdf4-python.googlecode.com)
+# requires netcdf4-python
 from netCDF4 import Dataset as NetCDFFile
 import numpy as np
 import matplotlib.pyplot as plt
 import random
 
-
+#--------------------------------------Open File-----------------------------------------
 nc = NetCDFFile('examples/file.nc')
 #Source
 #http://water.weather.gov/precip/
-
 prcpvar = nc.variables['amountofprecip']
 data = 0.01*prcpvar[:]
 
-#In order to put new values
+#----------------------------------Put news values------------------------------------
+#Simulate a value from MQTT
 for i in range (620,630):
     for j in range (754,766):
         data[i][j]=50
-
+#------------------------------------Map setting---------------------------------------------
 
 latcorners = nc.variables['lat'][:]
 loncorners = -nc.variables['lon'][:]
@@ -28,8 +28,6 @@ fig = plt.figure(figsize=(8,8))
 ax = fig.add_axes([0.1,0.1,0.8,0.8])
 
 m = Basemap(resolution='l',satellite_height=4000000.,projection='nsper',lat_0 = 35., lon_0 = -20.,llcrnrx=400000.,llcrnry=500000.,urcrnrx=2700000.,urcrnry=2700000.)
-
-
 # draw coastlines, state and country boundaries, edge of map.
 m.drawcoastlines()
 m.drawstates()
@@ -45,20 +43,10 @@ lons, lats = m.makegrid(nx, ny) # get lat/lons of ny by nx evenly space grid.
 x, y = m(lons, lats) # compute map proj coordinates.
 # draw filled contours.
 
-
+#-------------------------------------matplotlib setting ------------------------------------------
 #Select values on lader
-#clevs = [0,1,2.5,5,7.5,10,15,20,30,40,50,70,100,150,200,250,300,400,500,600,750]
-#clevs = []
 clevs=[0,1,2,3,4,5,7.5,10,12.5,15,20,25,30,35,40,50,75,100,200]
 
-'''
-for i in range(0,5):
-    clevs.append(i)
-for i in range(0,5):
-    clevs.append(5+2*i)
-for i in range(0,5):
-    clevs.append(15+5*i)
-'''
 
 cs = m.contourf(x,y,data,clevs,cmap=cm.s3pcpn)
 
